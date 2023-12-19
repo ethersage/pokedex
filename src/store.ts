73bf254a3799beabf0a54c14167d8784214bd2f1
Joshua Kaminetsky <jmkaminetsky@gmail.com>
2023-12-19 13:58:28 -0800

    async thunks

--- src/store.ts
+++ src/store.ts
@@ -1,31 +1,72 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ThunkAction } from 'redux-thunk';
 
+interface Pokemon {} // Define the Pokemon interface as per your requirement
+
+// Define the state type
+interface SearchState {
+  history: Set<string>;
+  results: Pokemon[];
+  status: 'idle' | 'loading' | 'error';
+  term: string;
+}
+
+// Initial State
+const initialState: SearchState = {
+  history: new Set(),
+  status: 'idle',
+  results: [],
+  term: '',
+};
+
+type StartSearchAction = PayloadAction<string>;
+type FulfillSearchAction = PayloadAction<Pokemon[]>;
+type RejectSearchAction = PayloadAction;
+
+type SearchActions =
+  | StartSearchAction
+  | FulfillSearchAction
+  | RejectSearchAction;
+
+// Create a slice for search functionality
 const searchSlice = createSlice({
   name: 'search',
-  initialState: {
-    history: new Set(),
-    isLoading: false,
-    results: [],
-    term: '',
-  },
+  initialState,
   reducers: {
-    load: (state, action) => {
-      const term = action.payload.term;
-
-      state.isLoading = true;
-      state.term = term;
-      state.history.add(term);
+    startSearch: (state, action: StartSearchAction) => {
+      state.status = 'loading';
+      state.term = action.payload;
     },
-    loaded: (state) => {
-      // fetch here
-
-      state.isLoading = false;
+    fulfillSearch: (state, action: FulfillSearchAction) => {
+      state.status = 'idle';
+      state.history.add(state.term);
+      state.results = action.payload;
+    },
+    rejectSearch: (state) => {
+      state.status = 'error';
     },
   },
 });
 
-export const { load, loaded } = searchSlice.actions;
+const fetchPokemon =
+  (name: string): ThunkAction<void, SearchState, null, SearchActions> =>
+  async (dispatch) => {
+    dispatch(searchSlice.actions.startSearch(name));
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+      const pokemon = await response.json();
+      dispatch(searchSlice.actions.fulfillSearch(pokemon));
+    } catch (error) {
+      dispatch(searchSlice.actions.rejectSearch());
+    }
+  };
 
-export const store = configureStore({
-  reducer: searchSlice.reducer,
+// Configure the store
+const store = configureStore({
+  reducer: {
+    search: searchSlice.reducer,
+  },
 });
+
+export { fetchPokemon };
+export default store;

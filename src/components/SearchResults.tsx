b02fa4ad784210b6f60bfc0b581ba14894477e77
Joshua Kaminetsky <jmkaminetsky@gmail.com>
2023-12-20 16:15:36 -0800

    capitalize name

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,9 +1,16 @@
 import { useSelector } from 'react-redux';
 
 import { SearchState } from '../store';
+import { useMemo } from 'react';
 
 export function SearchResults() {
   const search = useSelector((state: { search: SearchState }) => state.search);
+  const name = search.result?.name || '';
+  const upperName = useMemo(
+    () =>
+      name.length > 0 ? `${name[0].toUpperCase()}${name.substring(1)}` : '',
+    [search.result?.name]
+  );
 
   if (search.status === 'error') {
     // There could be other errors here, but we haven't accounted for that
@@ -17,8 +24,8 @@ export function SearchResults() {
   if (search.status === 'idle' && search.result !== null) {
     return (
       <>
-        <h2>{search.result.name}</h2>
-        <img src={search.result.imageUrl} alt={search.result.name} />
+        <h2>{upperName}</h2>
+        <img src={search.result.imageUrl} alt={upperName} />
       </>
     );
   }
